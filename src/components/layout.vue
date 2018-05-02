fad4a772a129eb75ac620cbe7c3fe045b7400785
alansif <alansif@sina.com>
2018-05-02 17:41:02 +0800

    go on

--- src/components/layout.vue
+++ src/components/layout.vue
@@ -9,16 +9,50 @@
         </el-form>
         <el-form :inline="true" ref="form1" :model="form1" label-width="60px" class="layout2">
             <el-form-item label="姓名">
-                <el-input v-model="form1.name" readonly="true"></el-input>
+                <el-input v-model="form1.name" readonly="true" style="width: 120px"></el-input>
             </el-form-item>
             <el-form-item label="性别">
-                <el-input v-model="form1.sex" readonly="true"></el-input>
+                <el-input v-model="form1.sex" readonly="true" style="width: 60px"></el-input>
             </el-form-item>
             <el-form-item label="电话">
-                <el-input v-model="form1.sex" readonly="true"></el-input>
+                <el-input v-model="form1.mobile" readonly="true" style="width: 195px"></el-input>
             </el-form-item>
             <el-form-item label="地址">
-                <el-input v-model="form1.sex" readonly="true"></el-input>
+                <el-input v-model="form1.address" readonly="true" style="width: 525px"></el-input>
+            </el-form-item>
+        </el-form>
+        <el-form :inline="true"  label-width="80px" class="layout3">
+            <el-form-item label="会员期限">
+                <el-input readonly="true" style="width: 60px" placeholder="3年"></el-input>
+            </el-form-item>
+            <el-form-item label="有效期">
+                <el-date-picker
+                        v-model="period"
+                        value-format="yyyy-MM-dd"
+                        type="daterange"
+                        :unlink-panels="true"
+                        :clearable="false"
+                        :editable="false"
+                        range-separator="至"
+                        start-placeholder="开始日期"
+                        end-placeholder="结束日期">
+                </el-date-picker>
+            </el-form-item>
+            <el-form-item label="最优价格">
+                <el-input
+                        placeholder="每年价格"
+                        style="width: 90px"
+                        onkeypress="return (event.charCode===46) || (event.charCode>=48 && event.charCode <=57)">
+                </el-input>
+            </el-form-item>
+            <el-form-item label="健康顾问">
+                <el-input style="width: 90px"></el-input>
+            </el-form-item>
+            <el-form-item label="定制电话">
+                <el-input style="width: 135px"></el-input>
+            </el-form-item>
+            <el-form-item label="备注">
+                <el-input style="width: 505px"></el-input>
             </el-form-item>
         </el-form>
     </div>
@@ -30,12 +64,18 @@
             return {
                 form1: {
                     idnumber: '333444',
-                    name: '',
-                    sex: '',
-                    mobile: '',
+                    name: '哈哈哈哈',
+                    sex: '男',
+                    mobile: '13800123456',
                     address: ''
-                }
+                },
+                period:['','']
             }
+        },
+        mounted() {
+            let d0 = moment().format('YYYY-MM-DD');
+            let d1 = moment().add('3','years').format('YYYY-MM-DD');
+            this.period = [d0, d1];
         },
         methods: {
             dosearch0() {
@@ -47,15 +87,22 @@
 
 <style>
     .layout1 {
-        width: 560px;
+        width: 590px;
         height: 44px;
+        background-color: white;
+        padding: 20px 30px 20px 20px;
+        margin-bottom: 12px;
+    }
+    .layout2 {
+        width: 600px;
+        height: 108px;
         background-color: white;
         padding: 20px 20px;
         margin-bottom: 12px;
     }
-    .layout2 {
-        width: 560px;
-        height: 108px;
+    .layout3 {
+        width: 600px;
+        height: 172px;
         background-color: white;
         padding: 20px 20px;
         margin-bottom: 12px;
