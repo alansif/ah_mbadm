6441e306c403771deb3c9ed624b04fb6d7946bc5
alansif <alansif@sina.com>
2018-07-19 21:17:22 +0800

    export csv

--- src/components/query2.vue
+++ src/components/query2.vue
@@ -33,7 +33,10 @@
                         style="width: 140px">
                 </el-date-picker>
             </el-form-item>
-            <el-button type="primary" :loading="loading" @click="doquery" style="width: 100px">查询</el-button>
+            <div style="text-align: center">
+                <el-button type="primary" :loading="loading" @click="doquery" style="width: 160px">查询</el-button>
+                <el-button @click="exportfile" :disabled="restbl.length === 0" style="width: 160px">导出{{restbl.length}}项</el-button>
+            </div>
         </el-form>
         <qtable ref="qtr2"></qtable>
     </div>
@@ -43,6 +46,78 @@
     import qtable from './qtable.vue';
     import {restbase,reserr} from '../restapi.js';
 
+    function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
+        //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
+        var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+
+        var CSV = '';
+        //Set Report title in first row or line
+
+//        CSV += ReportTitle + '\r\n\n';
+
+        //This condition will generate the Label/Header
+        if (ShowLabel) {
+            var row = "";
+
+            //This loop will extract the label from 1st index of on array
+            for (var index in arrData[0]) {
+
+                //Now convert each value to string and comma-seprated
+                row += index + ',';
+            }
+
+            row = row.slice(0, -1);
+
+            //append Label row with line break
+            CSV += row + '\r\n';
+        }
+
+        //1st loop is to extract each row
+        for (var i = 0; i < arrData.length; i++) {
+            var row = "";
+
+            //2nd loop will extract each column and convert it in string comma-seprated
+            for (var index in arrData[i]) {
+                row += '"' + arrData[i][index] + '",';
+            }
+
+            row.slice(0, row.length - 1);
+
+            //add a line break after each row
+            CSV += row + '\r\n';
+        }
+
+        if (CSV == '') {
+            alert("Invalid data");
+            return;
+        }
+
+        CSV = '\ufeff' + CSV;
+
+        //Generate a file name
+        var fileName = "";
+        //this will remove the blank-spaces from the title and replace it with an underscore
+        fileName += ReportTitle.replace(/ /g,"_");
+        fileName += '.csv';
+
+        var blob = new Blob([CSV], { type: 'text/csv;charset=utf-8;' });
+        if (navigator.msSaveBlob) { // IE 10+
+            navigator.msSaveBlob(blob, fileName);
+        } else {
+            var link = document.createElement("a");
+            if (link.download !== undefined) { // feature detection
+                // Browsers that support HTML5 download attribute
+                var url = URL.createObjectURL(blob);
+                link.setAttribute("href", url);
+                link.setAttribute("download", fileName);
+                link.style.visibility = 'hidden';
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            }
+        }
+    }
+
     export default {
         data() {
             return {
@@ -50,11 +125,13 @@
                 from1: '',
                 to0: '',
                 to1: '',
-                loading: false
+                loading: false,
+                restbl: []
             }
         },
         methods: {
             doquery() {
+                this.restbl = [];
                 this.loading = true;
                 this.$axios.get(restbase() + 'query/period',{params:{
                     from0: this.from0,
@@ -64,6 +141,7 @@
                 }}).then(response => {
                     this.loading = false;
                     const d = response.data.data;
+                    this.restbl = d;
                     this.$refs['qtr2'].showtable(d);
                 })
                 .catch(error => {
@@ -73,6 +151,9 @@
                         this.$message.error(reserr(error));
                     }
                 });
+            },
+            exportfile() {
+                JSONToCSVConvertor(this.restbl, 'export', true);
             }
         },
         components:{
@@ -83,9 +164,9 @@
 
 <style>
     .q2f1{
-        width: 930px;
+        width: 410px;
         background-color: white;
-        padding: 20px 20px 0 20px;
+        padding: 20px;
         margin-bottom: 8px;
     }
 </style>
