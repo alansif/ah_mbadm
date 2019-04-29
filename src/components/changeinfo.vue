f1c1efc78ed6a79c7e2a40c800316464f291c7d2
alansif <alansif@sina.com>
2019-04-29 16:26:34 +0800

    revise changeinfo

--- src/components/changeinfo.vue
+++ src/components/changeinfo.vue
@@ -16,6 +16,24 @@
                        @click="form1.agent=oldinfo.agent" v-show="form1.agent!==oldinfo.agent"></i>
                 </el-input>
             </el-form-item>
+            <el-form-item label="健康顾问" prop="advisor">
+                <el-input v-model="form1.advisor" style="width:200px">
+                    <i class="el-icon-d-arrow-left el-input__icon" slot="suffix"
+                       @click="form1.advisor=oldinfo.advisor" v-show="form1.advisor!==oldinfo.advisor"></i>
+                </el-input>
+            </el-form-item>
+            <el-form-item label="益生套餐" prop="ystc">
+                <el-input v-model="form1.ystc" style="width:200px">
+                    <i class="el-icon-d-arrow-left el-input__icon" slot="suffix"
+                       @click="form1.ystc=oldinfo.ystc" v-show="form1.ystc!==oldinfo.ystc"></i>
+                </el-input>
+            </el-form-item>
+            <el-form-item label="已用套餐" prop="yyystc">
+                <el-input v-model="form1.yyystc" style="width:200px">
+                    <i class="el-icon-d-arrow-left el-input__icon" slot="suffix"
+                       @click="form1.yyystc=oldinfo.yyystc" v-show="form1.yyystc!==oldinfo.yyystc"></i>
+                </el-input>
+            </el-form-item>
             <el-form-item label="定制电话" prop="altphone">
                 <el-input v-model="form1.altphone" style="width:265px">
                     <i class="el-icon-d-arrow-left el-input__icon" slot="suffix"
@@ -48,11 +66,17 @@
                 form1: {
                     agent: '',
                     altphone: '',
+					advisor: '',
+					ystc:'',    //益生套餐
+					yyystc:'',  //已用益生套餐
                     comment: ''
                 },
                 oldinfo: {
                     agent: '',
                     altphone: '',
+					advisor: '',
+					ystc:'',    //益生套餐
+					yyystc:'',  //已用益生套餐
                     comment: ''
                 },
                 showbaseinfo: false,
@@ -73,9 +97,15 @@
                     if (!err) {
                         this.form1.agent = data['会员经理'];
                         this.form1.altphone = data['定制电话'];
+                        this.form1.advisor = data['健康顾问'];
+                        this.form1.ystc = data['益生套餐'];
+                        this.form1.yyystc = data['已用益生套餐'];
                         this.form1.comment = data['备注'];
                         this.oldinfo.agent = data['会员经理'];
                         this.oldinfo.altphone = data['定制电话'];
+                        this.oldinfo.advisor = data['健康顾问'];
+                        this.oldinfo.ystc = data['益生套餐'];
+                        this.oldinfo.yyystc = data['已用益生套餐'];
                         this.oldinfo.comment = data['备注'];
                         this.showbaseinfo = true;
                         this.cando = true;
@@ -87,6 +117,15 @@
                 let chglist = [];
                 if (this.form1.agent!==this.oldinfo.agent) {
                     chglist.push({entry:'会员经理',value:this.form1.agent});
+                }
+                if (this.form1.advisor!==this.oldinfo.advisor) {
+                    chglist.push({entry:'健康顾问',value:this.form1.advisor});
+                }
+                if (this.form1.ystc!==this.oldinfo.ystc) {
+                    chglist.push({entry:'益生套餐',value:this.form1.ystc});
+                }
+                if (this.form1.yyystc!==this.oldinfo.yyystc) {
+                    chglist.push({entry:'已用益生套餐',value:this.form1.yyystc});
                 }
                 if (this.form1.altphone!==this.oldinfo.altphone) {
                     chglist.push({entry:'定制电话',value:this.form1.altphone});
